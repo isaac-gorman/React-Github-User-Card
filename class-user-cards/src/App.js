62f937df6a72fd03d77acf103f8ef297ab046e14
Isaac Gorman <emailme@isaacg.net>
2020-08-04 17:21:04 -0500

    initial commit

---
+++ class-user-cards/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h6>GitHub User Cards</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
